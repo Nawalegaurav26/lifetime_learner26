77b835749a4bd2a71f4d44d835fea5d1cc97ea5f
Nawalegaurav26 <nawalegaurav26@gmail.com>
2026-05-26 16:36:12 +0530

    feat: update src/app/posts/page.tsx

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
 
 export default function PostsPage() {
   return (
-    <main className="bg-slate-950 pt-32 min-h-screen">
+    <main className="bg-background pt-32 min-h-screen">
       <Posts />
     </main>
   );

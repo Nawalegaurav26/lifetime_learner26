134151bc74df41148d3fdaf588699f43503265fe
Nawalegaurav26 <nawalegaurav26@gmail.com>
2026-05-26 17:04:45 +0530

    seo: update domain and metadata in src/app/sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://lifetime-learner26.netlify.app'
+  const baseUrl = 'https://gaurav-nawale.vercel.app'
 
   return [
     {

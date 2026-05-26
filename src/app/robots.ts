fa5a38693304bb356a7114b9c812a6388fddf9fd
Nawalegaurav26 <nawalegaurav26@gmail.com>
2026-05-26 17:04:39 +0530

    seo: update domain and metadata in src/app/robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://lifetime-learner26.netlify.app/sitemap.xml',
+    sitemap: 'https://gaurav-nawale.vercel.app/sitemap.xml',
   }
 }

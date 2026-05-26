bfb7d1651dd1dd1ddb1ce538e3ed250c845a2977
Nawalegaurav26 <nawalegaurav26@gmail.com>
2026-05-26 17:04:04 +0530

    seo: update domain and metadata in src/app/posts/[slug]/page.tsx

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     openGraph: {
       title: `${title} | Gaurav Raju Nawale`,
       description: description,
-      url: `https://lifetime-learner26.netlify.app/posts/${slug}`,
+      url: `https://gaurav-nawale.vercel.app/posts/${slug}`,
       type: "article",
       images: [
         {
